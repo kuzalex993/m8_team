1677c52228ea0d9f455713476c826c6354343a75
Aleksej Kuzmin <kuzminale@sbsit.com>
2024-07-17 20:13:49 +0200

    Added function to check Telegram API connection

--- components/notifications.py
+++ components/notifications.py
@@ -9,6 +9,17 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 BASE_URL = f"https://api.telegram.org/bot{BOT_TOKEN}"
 
 
+def check_telegram_api():
+    url = "https://api.telegram.org"
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            print("Successfully connected to Telegram API")
+        else:
+            print(f"Failed to connect to Telegram API, status code: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
 def send_message(chat_id, text: str):
     if chat_id is None:
         print(f"chat_id is None - no notifications will be sent!")
@@ -23,5 +34,8 @@ def send_message(chat_id, text: str):
         except requests.exceptions.RequestException as e:
             print(f"An error occurred: {e}")
             return None
-        
+
+
+
+
 
