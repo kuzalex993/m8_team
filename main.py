3b48183c39f24b0865eb0df49c3f4a879be6298a
Aleksej Kuzmin <kuzminale@sbsit.com>
2024-07-20 15:58:30 +0200

    Rearrenged session variables and removed unused code

--- main.py
+++ main.py
@@ -8,8 +8,15 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-if "users_config" not in st.session_state or  st.session_state.users_config is None:
-    st.session_state.users_config = get_credentials()
+if "users_config" not in st.session_state or st.session_state["users_config"] is None:
+    st.session_state["users_config"] = get_credentials()
+if "bot_endpoint" not in st.session_state:
+    st.session_state["bot_endpoint"] = os.getenv("T_BOT_ENDPOINT")
+if "user_name" not in st.session_state:
+    st.session_state["user_name"] = None
+if "user_id" not in st.session_state:
+    st.session_state["user_id"] = None
+
 authenticator = stauth.Authenticate(
     st.session_state.users_config['credentials'],
     st.session_state.users_config['cookie']['name'],
@@ -17,13 +24,12 @@ authenticator = stauth.Authenticate(
     st.session_state.users_config['cookie']['expiry_days'],
     st.session_state.users_config['preauthorized']
 )
-if "bot_endpoint" not in st.session_state:
-    st.session_state["bot_endpoint"] = os.getenv("T_BOT_ENDPOINT")
-st.session_state.user_name, authentication_status, st.session_state.user_id = authenticator.login(location='main',
-                                                                            fields={'Form name': 'Войти в аккаунт',
-                                                                                    'Username': 'Имя пользователя',
-                                                                                    'Password': 'Пароль',
-                                                                                    'Login': 'Войти'})
+
+st.session_state["user_name"], authentication_status, st.session_state["user_id"] = authenticator.login(location='main',
+                                                                                                    fields={'Form name': 'Войти в аккаунт',
+                                                                                                            'Username': 'Имя пользователя',
+                                                                                                            'Password': 'Пароль',
+                                                                                                            'Login': 'Войти'})
 
 if authentication_status == False:
     st.error('Имя пользователя и/или пароль введены неверно')
@@ -48,45 +54,13 @@ if authentication_status is not True:
             st.error(e)
 
 if authentication_status is True:
-    if st.session_state.user_id == 'admin':
+    if st.session_state["user_id"] == 'admin':
         show_admin_page()
     else:
         show_user_page()
     authenticator.logout(button_name='Выйти',
                          location='sidebar')
     
-class User:
-    def __init__(self, name: str, email: str, position: str, role: str, free_bonuses: int = 0, reserved_bonuses: int = 0):
-        self.name = name
-        self.email = email
-        self.position = position
-        self.free_bonuses = free_bonuses
-        self.reserved_bonuses = reserved_bonuses
-        self.role = role
-    def __str__(self):
-        return str({
-            "user_name": self.name,
-            "user_email": self.email,
-            "user_position": self.position,
-            "user_free_bonuses": self.free_bonuses,
-            "user_reserved_bonuses": self.reserved_bonuses,
-            "user_role": self.role
-        })
-
-    @staticmethod
-    def from_dict(source):
-        pass
-
-    def to_dict(self):
-        return {
-            "user_name": self.name,
-            "user_email": self.email,
-            "user_position": self.position,
-            "user_free_bonuses": self.free_bonuses,
-            "user_reserved_bonuses": self.reserved_bonuses,
-            "user_role": self.role
-        }
-tmp = User("Федоров Федор","fedorovfedor@8agency.com", "project manager", "user", 450, 0)
 
 
 
