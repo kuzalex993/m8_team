1368f87c49d6ad6a32c6bd97515d8e4dc9274501
Aleksei <kuzminalekseidev@gmail.com>
2026-05-01 16:02:06 +0300

    Add '!'

--- src/m8_team/main.py
+++ src/m8_team/main.py
@@ -40,7 +40,7 @@ st.session_state["user_name"], authentication_status, st.session_state["user_id"
 )
 
 if not authentication_status:
-    st.error("Имя пользователя и/или пароль введены неверно")
+    st.error("Имя пользователя и/или пароль введены неверно!")
 elif authentication_status is None:
     st.warning("Введите имя пользователя и пароль")
 
